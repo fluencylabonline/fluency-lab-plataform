5f489d158fadf5f893b6af1dd70dc3e2dd132322
devmatheusfernandes <dev.matheusfernandes@gmail.com>
2026-04-10 17:47:46 -0300

    fix: organized the auth files and paths better

--- app/[locale]/template.tsx
+++ app/[locale]/template.tsx
@@ -1,26 +1,13 @@
 "use client";
 
+import { usePWA } from "@/hooks/ui/usePWA";
 import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
-
-interface NavigatorStandalone extends Navigator {
-  standalone?: boolean;
-}
 
 export default function Template({ children }: { children: React.ReactNode }) {
-  const [isPWA, setIsPWA] = useState(false);
-
-  useEffect(() => {
-    const checkPWA = () => {
-      const isStandalone = window.matchMedia('(display-mode: standalone)').matches
-        || (window.navigator as NavigatorStandalone).standalone;
-      setIsPWA(!!isStandalone);
-    };
-    checkPWA();
-  }, []);
+  const isStandalone = usePWA();
 
   const variants = {
-    hidden: isPWA ? { x: 30, opacity: 0 } : { opacity: 0 },
+    hidden: !isStandalone ? { x: 30, opacity: 0 } : { opacity: 0 },
     enter: { x: 0, opacity: 1 },
   };
 
